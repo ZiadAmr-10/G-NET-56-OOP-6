e15980802f93799a382f2a7d6531ff920965c83c
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-07-26 18:44:41 +0300

    Question01

--- Assignment06/Program.cs
+++ Assignment06/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+            #region Question01
+            /*
+            Abstraction:means hiding the implementation details and exposing only the essential features
+                        and  focuses on what object does not how does.
+            Encapsulation:means hiding the internal data and controlling access to it 
+                          and it focuses on protecting the object's data.
+            Example :  BankAccount
+            Abstract : The user can call method like deposit,withdraw,checkbalance without knowing how it does implementation or knowing the transaction
+            Encapsulation: Balance should be private can't access directly,should use properties/methods to access it to protect the data 
+             */
+            #endregion
+            #endregion
         }
     }
 }
